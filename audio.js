cf89e08d685611640363f22ff7a516b2ec9f8b3e
rickymartinez <55818232+rickymartinez@users.noreply.github.com>
2022-05-26 16:52:19 +0700

    Add files via upload

---
+++ audio.js
@@ -0,0 +1,13 @@
+var aud  =  $("audio") [0]
+$(".play-pause"). on("click", function () {
+    if (aud. paused)    {
+        aud. play () ;
+        $(".play-pause"). removeClass ("icon-play") ;
+        $(".play-pause"). addClass ("icon-stop") ;
+    }
+     else {
+         aud. pause() ;
+         $(".play-pause"). removeClass ("icon-stop") ;
+         $(".play-pause"). addClass ("icon-play") ;
+     }
+})
